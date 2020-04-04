d76ea460acb7285b58be495051a7f7806166eeed
tpshan <tinat6608@gmail.com>
2020-04-05 03:15:08 +0800

    adjust code

--- src/scripts/parts/tomato.js
+++ src/scripts/parts/tomato.js
@@ -2,21 +2,38 @@ import $ from 'jquery';
 import { setFinishTodolist, getTodolistByStatus, getTodolist } from './todolist';
 
 function init(){
-    //輸入 id 則會將該id的status改成true,成功回傳true,失敗回傳false
-    
-    //------要整理code
+    //window.onload = nowDate;
+    $(function(){nowDate();});
     
     outputTodolist();
-    $("#add-button").on("click", function() {
-        outputTodolist();
-    });
-
-    //window.onload = nowDate;
-    $(function(){nowDate();})
+    $("#add-button").on("click", function() { outputTodolist(); });
+    
     $(".green").hide();
     $("#bellSlash").hide();
-}
+    
+    $("#bell").click(function() { bellfunc(1); });
+    $("#bellSlash").click(function() { bellfunc(2); });
 
+    $("#orangePlay").click(function() { orangeChangeState(1); });
+    $("#orangePause").click(function() { orangeChangeState(2); });
+
+    $("#greenPlay").click(function() { greenChangeState(1); });
+    $("#greenPause").click(function() { greenChangeState(2); });
+    
+    $("#orangeCancel").click(function() { greenMode(); });
+    $("#greenCancel").click(function() { orangeMode(); });
+
+    $("#finishCheckONE").click(function() { setFinish(0); });
+    $("#finishCheckTWO").click(function() { setFinish(1); });
+    $("#finishCheckTHREE").click(function() { setFinish(2); });
+    $("#finishCheckFOUR").click(function() { setFinish(3); });
+
+}
+var countDownNumber = 1500;
+var countDownID;
+var countDownGoing = 0;
+var countDownTime = $("#clock");
+var orangeGreenMode = true;     //true = its orange(default) mode now
 
 function nowDate() {
     var nowDatetime = new Date();
@@ -33,32 +50,22 @@ function nowDate() {
     setTimeout(nowDate, 1000);
 }
 
-//----------count down timer-----------
-var countDownNumber = 1500;
-var countDownID;
-var countDownGoing = 0;
-var countDownTime = $("#clock");
-var orangeGreenMode = true;     //true = its orange(default) mode now
+function plusZero (param) {
+    return (param < 10 ? '0' : '');
+}
 
 function countDownfunc() {
-
     if (countDownNumber == 0) {
-        //alert("倒數結束");
         countDownTime.html("00:00");
         clearTimeout(countDownID);
         if (orangeGreenMode) {
-            countDownNumber = 300;
-            countDownTime.html("05:00");
-            orangeGreenMode = false;
             greenMode();
-        }else {
-            countDownNumber = 1500;
-            countDownTime.html("25:00");
-            orangeGreenMode = true;
+        }
+        else {
             orangeMode();
         }
-        
-    }else {
+    }
+    else {
         countDownNumber--;
         var countDownMin = Math.floor(countDownNumber / 60);
         var countDownSec = countDownNumber % 60
@@ -75,19 +82,20 @@ function startCount() {
         countDownfunc();
     }
 }
+
 function stopCount() {
     clearTimeout(countDownID);
     countDownGoing = 0;
 }
 
-//---------initial and bell---------
-
-$("#bell").click(function() {
-    bellfunc(1);
-});
-$("#bellSlash").click(function() {
-    bellfunc(2);
-});
+function orangeMode() {
+    countDownNumber = 1500;
+    countDownTime.html("25:00");
+    stopCount();
+    $(".orange").show();
+    $(".green").hide();
+    orangeGreenMode = true;
+}
 
 function bellfunc(bellState) {
     if(bellState == 1) {
@@ -100,108 +108,65 @@ function bellfunc(bellState) {
     }
 }
 
-//---------orange mode------------
-
-function orangeMode() {
-    $(".orange").show();
-    $(".green").hide();
-}
-$("#orangePlay").click(function() {
-    // some feature
-    orangeChangeState(1);
-    countDownGoing = 0;
-    startCount();
-});
-
-$("#orangePause").click(function() {
-    orangeChangeState(2);
-    stopCount();
-});
 function orangeChangeState(param) {
     if(param == 1) {
         $("#orangePause").show();
         $("#orangePlay").hide();
+        countDownGoing = 0;
+        startCount();
     }
     else {
         $("#orangePause").hide();
         $("#orangePlay").show();
+        stopCount();
     }
 }
-$("#orangeCancel").click(function() {
+
+function greenMode() {
     countDownNumber = 300;
     countDownTime.html("05:00");
     stopCount();
-    greenMode();
-});
-
-//------------green mode----------------
-
-function greenMode() {
     $(".green").show();
     $(".orange").hide();
+    orangeGreenMode = false;
 }
 
-$("#greenPlay").click(function() {
-    greenChangeState(1);
-    countDownGoing = 0;
-    startCount();
-});
-$("#greenPause").click(function() {
-    greenChangeState(2);
-    stopCount();
-});
 function greenChangeState(param) {
     if(param == 1) {
         $("#greenPause").show();
         $("#greenPlay").hide();
+        countDownGoing = 0;
+        startCount();
     }
     else {
         $("#greenPause").hide();
         $("#greenPlay").show();
+        stopCount();
     }
 }
-$("#greenCancel").click(function() {
-    countDownNumber = 1500;
-    countDownTime.html("25:00");
-    stopCount();
-    orangeMode();
-});
 
 function outputTodolist() {     //NULL 時不顯示 、一定要重新整理，無法自動更新(刪除、完成時) (新增可以
     var notFinish = getTodolistByStatus(0);
-    
     for (var i = 0; i < notFinish.length; i++) {
         var readObject = notFinish[i];
         $.each(readObject, function(key, value) {
            if (key == "name") {
-                    if (i == 0) {
-                        $("#listONE").html(value);
-                    }
-                    else if (i == 1) {
-                        $("#listTWO").html(value);
-                    }
-                    else if (i == 2) {
-                        $("#listTHREE").html(value);
-                    }
-                    else if (i == 3) {
-                        $("#listFOUR").html(value);
-                    }
+                if (i == 0) {
+                    $("#listONE").html(value);
+                }
+                else if (i == 1) {
+                    $("#listTWO").html(value);
+                }
+                else if (i == 2) {
+                    $("#listTHREE").html(value);
+                }
+                else if (i == 3) {
+                    $("#listFOUR").html(value);
+                }
             }
         });
     }
 }
-$("#finishCheckONE").click(function() {
-    setFinish(0);
-});
-$("#finishCheckTWO").click(function() {        
-    setFinish(1);
-});
-$("#finishCheckTHREE").click(function() {        
-    setFinish(2);
-});
-$("#finishCheckFOUR").click(function() {        
-    setFinish(3);
-});
 
 function setFinish(whichOneFinish) {
     var notFinish = getTodolistByStatus(0);
